35620068795b6e5e3e9193a7bdf945ff26a8af42
skipeverylunch <harima.yasuhiro@outlook.jp>
2021-09-26 15:43:27 +0900

    animated About Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,75 @@
 <template>
   <div class="about">
-    <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <transition appear @before-enter="beforeEnter" @enter="enter">
+      <h1 class="mb-5 text-xl">About</h1>
+    </transition>
+    <transition appear name="paragraph">
+      <div v-if="showP">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+          officia possimus delectus inventore quod quisquam culpa voluptas
+          iusto, quae maiores quo dolorum, corporis laboriosam a dolore
+          consequatur assumenda nam!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+          officia possimus delectus inventore quod quisquam culpa voluptas
+          iusto, quae maiores quo dolorum, corporis laboriosam a dolore
+          consequatur assumenda nam!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+          officia possimus delectus inventore quod quisquam culpa voluptas
+          iusto, quae maiores quo dolorum, corporis laboriosam a dolore
+          consequatur assumenda nam!
+        </p>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-
+import gsap from 'gsap';
+import { ref } from 'vue';
 export default {
   setup() {
-
-  }
-}
+    const showP = ref(false);
+    const beforeEnter = (el) => {
+      gsap.from(el, {
+        y: -30,
+        opacity: 0,
+      });
+    };
+    const enter = (el, done) => {
+      gsap.to(el, {
+        duration: 0.5,
+        y: 0,
+        opacity: 1,
+        onComplete: done,
+      });
+      setTimeout(() => {
+        showP.value = true;
+      }, 250);
+    };
+    return {
+      beforeEnter,
+      enter,
+      showP,
+    };
+  },
+};
 </script>
 
 <style>
-  .about {
-    max-width: 600px;
-    margin: 20px auto;
-  }
+.about {
+  max-width: 600px;
+  margin: 20px auto;
+}
+.paragraph-enter-from {
+  transform: translateX(30px);
+  opacity: 0;
+}
+.paragraph-enter-active {
+  transition: all 0.5s ease;
+}
 </style>
