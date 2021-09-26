a2d102c689b2378a8f057106156ce1a17cc857e5
skipeverylunch <harima.yasuhiro@outlook.jp>
2021-09-26 15:59:07 +0900

    animated Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,48 +1,65 @@
 <template>
   <div class="contact">
     <h1>Contact</h1>
-    <ul>
-      <li v-for="icon in icons" :key="icon.name">
+    <transition-group
+      tag="ul"
+      appear
+      @before-enter="beforeEnter"
+      @enter="enter"
+    >
+      <li v-for="(icon, idx) in icons" :key="icon.name" :data-idx="idx">
         <span class="material-icons">{{ icon.name }}</span>
         <div>{{ icon.text }}</div>
       </li>
-    </ul>
+    </transition-group>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import gsap from 'gsap';
 
 export default {
   setup() {
     const icons = ref([
-      { name: 'alternate_email', text: 'by email'},
-      { name: 'local_phone', text: 'by phone'},
-      { name: 'local_post_office', text: 'by post'},
-      { name: 'local_fire_department', text: 'by smoke signal'},
-    ])
-
-    return { icons }
-  }
-}
+      { name: 'alternate_email', text: 'by email' },
+      { name: 'local_phone', text: 'by phone' },
+      { name: 'local_post_office', text: 'by post' },
+      { name: 'local_fire_department', text: 'by smoke signal' },
+    ]);
+    const beforeEnter = (el) => {
+      el.style.transform = 'translateY(60px)';
+      el.style.opacity = 0;
+    };
+    const enter = (el) => {
+      gsap.to(el, {
+        y: 0,
+        opacity: 1,
+        duration: 0.5,
+        delay: el.dataset.idx * 0.2,
+      });
+    };
+    return { beforeEnter, enter, icons };
+  },
+};
 </script>
 
 <style>
-  .contact ul {
-    padding: 0;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 20px;
-    max-width: 400px;
-    margin: 60px auto;
-  }
-  .contact li {
-    list-style-type: none;
-    background: white;
-    padding: 30px;
-    border-radius: 10px;
-    box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
-    cursor: pointer;
-    line-height: 1.5em;
-  }
+.contact ul {
+  padding: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  max-width: 400px;
+  margin: 60px auto;
+}
+.contact li {
+  list-style-type: none;
+  background: white;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  line-height: 1.5em;
+}
 </style>
